e634072b15792eeffdbe7fa7c3b410e6657e597d
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2021-07-05 10:20:57 -0500

    fix

--- lang.py
+++ lang.py
@@ -4,9 +4,69 @@ def ast():
         cmd = input("*> ")
         if cmd == "*": print("Hello World")
         elif cmd == " * ": print(random() * 2147483647)
-        elif cmd == "**": print("Who can say where the road goes"), print("Where the day flows, only time"), print("And who can say if your love grows"), print("As your heart chose, only time")
+        elif cmd == "**": print("""Who can say where the road goes
+Where the day flows, only time
+And who can say if your love grows
+As your heart chose, only time
+Who can say why your heart sighs
+As your love flies, only time
+And who can say why your heart cries
+When your love lies, only time
+Who can say when the roads meet
+That love might be in your heart
+And who can say when the day sleeps
+If the night keeps all your heart
+Night keeps all your heart
+Who can say if your love grows
+As your heart chose, only time
+And who can say where the road goes
+Where the day flows, only time
+Who knows? Only time
+Who knows? Only time""")
 
         elif cmd == "*+*":
             while True: pass
-            while False: print("Even a million scars doesn't change whose you are"), print("You're worthy"), print("Beautifully broken")
-File_object = open(r"example.txt","r")
+            while False: print("""Every tear, every doubt
+Every time you've fallen down
+When you're hurting, feeling ashamed
+When you're numbing off your pain
+When you've lost your way
+And feel so far away
+You're not
+You're beautifully broken
+And you can be whole again
+Even a million scars doesn't change whose you are
+You're worthy
+Beautifully broken
+Every fear of being loved
+For who you are no matter what
+When you're stumbling,
+And you're haunted by regret
+And the darkness closes in
+Just listen
+You're beautifully broken
+And You can be whole again
+Even a million scars doesn't change whose you are
+You're worthy
+Beautifully broken
+You're beautifully broken
+You're beautifully broken
+Oh, the God who made the stars
+Is the God that made your heart
+And He's holding you right now
+He can heal the broken parts
+And make beauty from the scars, the scars
+Beautiful scars!
+You're beautifully broken
+And You can be whole again
+Even a million scars doesn't change whose you are
+You're worthy
+You're beautifully broken
+And You can be whole again
+Even a million scars doesn't change whose you are
+You're worthy
+You're beautifully broken
+Beautifully broken
+You're beautifully broken""")
+#This currently dosen't work.
+#File_object = open(r"example","r")
